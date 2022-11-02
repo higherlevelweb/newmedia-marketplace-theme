cfaab58a7d2c6627035dea1021e19f4d4a435573
higherlevelweb <higherlevelweb@live.com>
2022-11-02 00:53:21 -0400

    Starter theme files

---
+++ footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package newmedia-marketplace
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				echo 'Theme by: <a href="http://higherlevelweb.com">Higher Level Web</a>';
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
