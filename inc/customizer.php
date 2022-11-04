e25a31c8eee0014be5d48a91652e0fadde91e79a
higherlevelweb <higherlevelweb@live.com>
2022-11-03 22:46:12 -0400

    Template includes for custom-header, customizer, jetpack, template-functions, template-tags, and woocommerce

---
+++ inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * newmedia-marketplace Theme Customizer
+ *
+ * @package newmedia-marketplace
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function newmedia_marketplace_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'newmedia_marketplace_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'newmedia_marketplace_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'newmedia_marketplace_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function newmedia_marketplace_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function newmedia_marketplace_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function newmedia_marketplace_customize_preview_js() {
+	wp_enqueue_script( 'newmedia-marketplace-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'newmedia_marketplace_customize_preview_js' );
